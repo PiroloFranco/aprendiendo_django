61e90485f397e4def1a106e5aafaa10bb3d187cf
Franco <pirolofip@gmail.com>
2025-07-14 22:00:21 -0300

    segundo commit

--- movie/admin.py
+++ movie/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from movie.models import Movie
 # Register your models here.
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ("title", "duration")
+
+
+admin.site.register(Movie, MovieAdmin)
