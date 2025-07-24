ef5624651aa3d77ada2b1e99f0f0cb317b673591
Franco <pirolofip@gmail.com>
2025-07-24 20:43:16 -0300

    no se}

--- primer_proyecto_django/settings.py
+++ primer_proyecto_django/settings.py
@@ -43,7 +43,11 @@ INSTALLED_APPS = [
     'user'
 ]
 
-REST_FRAMEWORK = {}
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES':[
+        'rest_framework_simplejwt.authentication.JWTAuthentication'
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
